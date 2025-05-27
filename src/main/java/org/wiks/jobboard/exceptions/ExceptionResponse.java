9a42b9e777694ac89e5b5027bc3a4a2c9a8a376d
wiktorry <wiktorszulc4@gmail.com>
2025-05-27 14:44:33 +0200

    feat: add exceptions handler

---
+++ src/main/java/org/wiks/jobboard/exceptions/ExceptionResponse.java
@@ -0,0 +1,5 @@
+package org.wiks.jobboard.exceptions;
+
+public record ExceptionResponse(String message, int status, long timeStamp) {
+
+}
---
+++ src/main/java/org/wiks/jobboard/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.wiks.jobboard.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
